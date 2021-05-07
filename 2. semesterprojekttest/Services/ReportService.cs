e087498aa78c2d8ecfad99759d11f9ddd9159d81
Alexander Grydgaard <alex043t@edu.easj.dk>
2021-05-07 11:54:35 +0200

    Reportsystem Netitity

---
+++ 2. semesterprojekttest/Services/ReportService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using _2._semesterprojekttest.Interfaces;
+using _2._semesterprojekttest.Models;
+
+namespace _2._semesterprojekttest.Services
+{
+    public class ReportService : IReportService
+    {
+        private GrydenDBContext db = new GrydenDBContext();
+
+        public void AddReport(Report report)
+        {
+            db.Reports.Add(report);
+            db.SaveChanges();
+        }
+    }
+}
