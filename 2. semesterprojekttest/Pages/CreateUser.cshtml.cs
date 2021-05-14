067a641654fbe2fc5f09a3518ef0e61620eaf526
Asmus Skafte Böttcher <asmu0543@edu.easj.dk>
2021-05-14 09:34:18 +0200

    RouteChanges

--- 2. semesterprojekttest/Pages/CreateUser.cshtml.cs
+++ 2. semesterprojekttest/Pages/CreateUser.cshtml.cs
@@ -7,11 +7,25 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using _2._semesterprojekttest.Services;
 using _2._semesterprojekttest.Models;
 using _2._semesterprojekttest.Interfaces;
+using Microsoft.AspNetCore.Http;
 
 namespace _2._semesterprojekttest.Pages
 {
     public class CreateUserModel : PageModel
     {
+        public int validUser
+        {
+            get { return Convert.ToInt32(HttpContext.Session.GetInt32("Login")); }
+        }
+        public int userID
+        {
+            get { return Convert.ToInt32(HttpContext.Session.GetInt32("UserID")); }
+        }
+        public int validDriver
+        {
+            get { return Convert.ToInt32(HttpContext.Session.GetInt32("Driver")); }
+        }
+
         private IUserService _userService;
 
         public CreateUserModel(IUserService service)
